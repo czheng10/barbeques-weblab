fe1b3a04808d433b2a5354ed846ed13b579cf03f
Amanda Zheng <ayzheng@mit.edu>
2022-01-10 16:29:52 -0500

    did some basic navbar

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -1,15 +1,23 @@
 import React from "react";
-import logo from "logo2.jpeg";
 import "./NavBar.css";
-console.log(logo);
-function NavBar() {
-    return (<nav className="navbar">
-        <div><img src = {logo} alt = "Logo"/></div>
-        <div className="navbar__name">Barbeque</div>
-        <div className="navbar__search">Search</div>
-        <div className="navbar__item">Notifications</div>
-        <div className="navbar__item">Username</div>
-    </nav>);
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, NavbarBrand, Form, FormControl, Button } from 'react-bootstrap';
+const logo = require('../../images/logo2.jpg');
+const NavBar = () => {
+    return (<Navbar>
+        <img src={logo.default} className="logo"/>
+        <NavbarBrand>Barbeque</NavbarBrand>
+        <Form className="d-flex search">
+        <input
+          type="text"
+          name="text"
+          placeholder="search users..."
+          className="bg-white p-2 w-3/4 outline-none"
+        />
+        <Button>Search</Button>
+      </Form>
+        <NavItem className="notifs">Notifications</NavItem>
+        <NavItem className="profile">Username </NavItem>
+    </Navbar>);
 };
 
 export default NavBar;
