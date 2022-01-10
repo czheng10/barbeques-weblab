7990c64996b5260076cfa8eabbebc7d42eb128e8
Amanda Zheng <ayzheng@mit.edu>
2022-01-10 13:20:47 -0500

    dummy pic

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -1,9 +1,10 @@
 import React from "react";
-
-import "./NavBar.css"
-const NavBar = () => {
+import logo from "logo2.jpeg";
+import "./NavBar.css";
+console.log(logo);
+function NavBar() {
     return (<nav className="navbar">
-        <div className="navbar__logo"></div>
+        <div><img src = {logo} alt = "Logo"/></div>
         <div className="navbar__name">Barbeque</div>
         <div className="navbar__search">Search</div>
         <div className="navbar__item">Notifications</div>
