6ffc5877d807c6000d57ce495b09d45b1f975965
Peihua Huang <peihua@mit.edu>
2022-01-17 01:01:29 -0500

    Removed skeleton page and update it with Home page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Router, navigate } from "@reach/router";
 import NotFound from "./pages/NotFound.js";
 import NavBar from "./modules/NavBar.js";
-import Skeleton from "./pages/Skeleton.js";
 import Profile from "./pages/Profile.js";
 import Home from "./pages/Home.js";
 import SearchResult from "./pages/SearchResult.js";
@@ -54,9 +53,8 @@ const App = () => {
     <>
       <NavBar handleLogin={handleLogin} handleLogout={handleLogout} userId={userId} user={user} />
       <Router>
-        <Skeleton path="/" />
         <Profile path="/profile/:targetUserId" userId={userId} />
-        <Home path="/home/" />
+        <Home path="/" />
         <SearchResult path="/search/:searchPhrase" userId={userId} />
         <SearchResult path="/search/" userId={userId} />
         <NotFound default />
