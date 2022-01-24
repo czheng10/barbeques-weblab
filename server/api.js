a8cc34ea05b764df2497d19bce49e0761a75b6e9
czheng10 <czheng10@stuy.edu>
2022-01-24 00:04:56 -0500

    styled notifs

--- server/api.js
+++ server/api.js
@@ -137,7 +137,7 @@ router.post(`/addPics`, auth.ensureLoggedIn, (req, res) => {
     src: req.body.newPic,
     title: req.body.picTitle,
     caption: req.body.picCap,
-  }
+  };
   User.findById(req.body.userid).then((results) => {
     results.pictures = results.pictures.concat(picCard);
     results.save().then((person) => res.send(person));
@@ -146,7 +146,7 @@ router.post(`/addPics`, auth.ensureLoggedIn, (req, res) => {
 
 router.post(`/removePics`, auth.ensureLoggedIn, (req, res) => {
   User.findById(req.body.userid).then((results) => {
-    results.pictures.splice(req.body.index,1);
+    results.pictures.splice(req.body.index, 1);
     results.save().then((person) => res.send(person));
   });
 });
@@ -220,6 +220,7 @@ router.post("/update-notif", auth.ensureLoggedIn, (req, res) => {
   User.findById(req.body.notifTo).then((user) => {
     user.notifs = user.notifs.filter((notif) => notif._id.toString() !== req.body.notifId);
     user.save().then((updated) => {
+      console.log(updated);
       res.send(updated.notifs);
     });
   });
