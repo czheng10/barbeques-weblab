6fa972534d892339d2ea92bdfee5188c7ad18f05
czheng10 <czheng10@stuy.edu>
2022-01-25 20:47:44 -0500

    styled party

--- client/src/components/pages/Party.js
+++ client/src/components/pages/Party.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 import { get, post } from "../../utilities";
+import bbq from "../../images/BarbequeLogo.png";
+import bbq2 from "../../images/logob.png";
 import "./Party.css";
 import { Link } from "@reach/router";
 
-const Party = ({location, userId, partyId }) => {
+const Party = ({ location, userId, partyId }) => {
   const [user, setUser] = useState(null);
   const [party, setParty] = useState("");
   const [members, setMembers] = useState([]);
@@ -28,7 +30,7 @@ const Party = ({location, userId, partyId }) => {
       setMembers(result.map((users) => users.name));
     });
   }, []);
-  
+
   if (!userId) {
     return <div>Please login first</div>;
   }
@@ -38,24 +40,48 @@ const Party = ({location, userId, partyId }) => {
   }
 
   const closeParty = () => {
-    post("/api/close", {partyid: partyId}).then((result) => {
-    });
-  }
+    post("/api/close", { partyid: partyId }).then((result) => {});
+  };
   return (
-    <>
-      <h1 className="u-textCenter">Party Summary: {party}</h1>
-      <p>
-        <b>Host: {user} </b> 
-        <Link
-              to={userId ? `/profile/${userId}` : "/"}
-            >
-              <button hidden = {location.state.show.showButtons} onClick = {() => closeParty()}>Close Party</button>
-              </Link>
-      </p>
-      <h2>Members:</h2>
-      {members.length === 0 ? <h4>No Members So Far</h4>: members.map((item, index) => 
-        <h5 key = {index}>{item}</h5>)}
-    </>
+    <div className="party-all">
+      <div className="">
+        <h1 className="u-textCenter party-header">
+          <span className="partysummary"> Party: </span>
+          <span className="partyname">{party}</span>
+        </h1>
+      </div>
+      <div className="hostContainer">
+        <p>
+          <h2>
+            <span className="partytitle"> Host: </span> {user}
+          </h2>
+          <img className="hostpic" src={bbq} alt="logo" />
+          <div>
+            <Link to={userId ? `/profile/${userId}` : "/"}>
+              <button
+                className="btn"
+                hidden={location.state.show.showButtons}
+                onClick={() => closeParty()}
+              >
+                End Party
+              </button>
+            </Link>
+          </div>
+        </p>
+      </div>
+      <div className="party-members">
+        <h2 className="partymembersheader partytitle ">Members:</h2>
+        {members.length === 0 ? (
+          <h6>No Members So Far</h6>
+        ) : (
+          members.map((item, index) => (
+            <div className="members">
+              <h5 key={index}>{item}</h5>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
   );
 };
 
